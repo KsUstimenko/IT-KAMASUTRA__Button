b2f84ee4678aa1ca5a7e96a45d661247be01c4f2
kseniia <ksusha.mamatova98@gmail.com>
2022-08-18 18:47:13 +0300

    third step

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import './App.css';
+import {Button} from "./Components/Button";
 
 function App() {
 
-    const foo1 = () => {
-        console.log(100200)
+    const Button1Foo = (subscriber: string, age: number, address: string) => {
+        console.log(subscriber, age, address)
     }
 
-    const foo2 = (num: number) => {
-        console.log(num)
+    const Button2Foo = (subscriber: string) => {
+        console.log(subscriber)
     }
 
-    return <div>
-        <button onClick={foo1}>1</button>
-        <button onClick={() => foo2(100200)}>2</button>
-    </div>
+    const Button3Foo = () => {
+        console.log('I am stupid button')
+    }
+
+    return (
+        <div>
+            <Button name={'MyYouTubeChanel-1'} callBack={() => Button1Foo('I am Vasya', 21, 'live in Minsk')}/>
+            <Button name={'MyYouTubeChanel-2'} callBack={() => Button2Foo('I am Ivan')}/>
+            <Button name={'Stupid Button'} callBack={Button3Foo}/>
+        </div>
+    );
 }
 
 export default App;
